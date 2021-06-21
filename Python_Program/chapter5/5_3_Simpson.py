f4d6d272a4b9b160b2b8ec9f93f4ee5bf98b226a
Montri Maleewong <31563109+maleewong@users.noreply.github.com>
2021-06-21 15:29:26 +0700

    Add files via upload

---
+++ Python_Program/chapter5/5_3_Simpson.py
@@ -0,0 +1,23 @@
+def simpson(f, a, b, n):
+  '''
+  f : function
+  a : number  : start
+  b : number  : stop
+  n : integer : must be even
+  '''
+  h = (b - a) / n
+  sum1 = sum(f(a + (2*i + 2)*h) for i in range(0, int(n/2) - 1))
+  sum2 = sum(f(a + (2*i + 1)*h) for i in range(0, int(n/2)))
+  return (f(a) + 2*sum1 + 4*sum2 + f(b)) * h / 3
+
+if __name__ == '__main__':
+  from math import cos, pi
+  f = lambda x : cos(x)
+  a = 0
+  b = pi / 2
+  n = 20
+  print(simpson(f, a, b, n))
+
+# author: Worasait Suwannik
+# date: May 2015
+
